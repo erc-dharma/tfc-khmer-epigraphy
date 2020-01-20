4783c5f67e6e661cca27949dfc4cb516cb196b92
ajaniak <33328906+ajaniak@users.noreply.github.com>
2020-01-20 15:03:17 +0100

    Convert to UTF-8 with LF ending

--- texts/CIK-TXT/convert.sh
+++ texts/CIK-TXT/convert.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-find ./ -type f -exec sed -i -e 's/\r̥̄/r̥̄/g' {} \;
-find ./ -type f -exec sed -i -e 's/\l̥̄/l̥̄/g' {} \;
-find ./ -type f -exec sed -i -e 's/\r̥/r̥/g' {} \;
-find ./ -type f -exec sed -i -e 's/\l̥/l̥/g' {} \;
-find ./ -type f -exec sed -i -e 's/ā/ā/g' {} \;
-find ./ -type f -exec sed -i -e 's/ī/ī/g' {} \;
-find ./ -type f -exec sed -i -e 's/ū/ū/g' {} \;
-find ./ -type f -exec sed -i -e 's/·/·/g' {} \;
-find ./ -type f -exec sed -i -e 's/\ṁ/ṁ/g' {} \;
-find ./ -type f -exec sed -i -e 's/ṅ/ṅ/g' {} \;
-find ./ -type f -exec sed -i -e 's/ñ/ñ/g' {} \;
-find ./ -type f -exec sed -i -e 's/\ṇ/ṇ/g' {} \;
-find ./ -type f -exec sed -i -e 's/\ṭ/ṭ/g' {} \;
-find ./ -type f -exec sed -i -e 's/\ḍ/ḍ/g' {} \;
-find ./ -type f -exec sed -i -e 's/ś/ś/g' {} \;
-find ./ -type f -exec sed -i -e 's/\ṣ/ṣ/g' {} \;
-find ./ -type f -exec sed -i -e 's/ẖ/ẖ/g' {} \;
-find ./ -type f -exec sed -i -e 's/ẖ k/ẖ k/g' {} \;
-find ./ -type f -exec sed -i -e 's/ẖk/ẖk/g' {} \;
-find ./ -type f -exec sed -i -e 's/ḫ/ḫ/g' {} \;
-find ./ -type f -exec sed -i -e 's/ḫ p/ḫ p/g' {} \;
-find ./ -type f -exec sed -i -e 's/ḫp/ḫp/g' {} \;
-find ./ -type f -exec sed -i -e 's/m̐/m̐/g' {} \;
-find ./ -type f -exec sed -i -e 's/m̃/m̃/g' {} \;
-find ./ -type f -exec sed -i -e 's/\ḥ/ḥ/g' {} \;
-find ./ -type f -exec sed -i -e 's/⏑/⏑/g' {} \;
-
-
+find ./txt/ -type f -exec sed -i -e 's/\.rr/r̥̄/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.ll/l̥̄/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.r/r̥/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.l/l̥/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/aa/ā/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/ii/ī/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/uu/ū/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/_/·/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.m/ṁ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/"n/ṅ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/~n/ñ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.n/ṇ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.t/ṭ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.d/ḍ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/"s/ś/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.s/ṣ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/:h/ẖ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/x k/ẖ k/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/xk/ẖk/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/;h/ḫ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/f p/ḫ p/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/fp/ḫp/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/;m/m̐/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/~m/m̃/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/\.h/ḥ/g' {} \;
+find ./txt/ -type f -exec sed -i -e 's/~/⏑/g' {} \;
